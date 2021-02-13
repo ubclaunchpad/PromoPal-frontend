44f8cb3a93a6eabdaa8bf96047a2c7defccc446b
ericrkuo <ericrkuo@gmail.com>
2021-02-12 23:55:53 -0800

    PP-66 Apply PR comments

--- src/components/promotion/PromotionDetails.tsx
+++ src/components/promotion/PromotionDetails.tsx
@@ -1,7 +1,7 @@
 import React, { CSSProperties, ReactElement } from 'react';
 import { HeartOutlined } from '@ant-design/icons';
 import { Promotion } from '../../types/promotion';
-import parse from '../../../node_modules/html-react-parser';
+import parse from 'html-react-parser';
 
 const styles: { [identifier: string]: CSSProperties } = {
   descriptionContainer: {
@@ -23,17 +23,17 @@ export default function PromotionDetails({
   description,
   expirationDate,
   restaurantName = 'Restaurant',
-  boldName = undefined,
-  boldDescription = undefined,
+  boldName,
+  boldDescription,
 }: Promotion): ReactElement {
   return (
     <div style={styles.descriptionContainer}>
       <div style={styles.header}>
-        {boldName !== undefined ? parse(boldName) : <h2>{name}</h2>}
+        {boldName ? parse(boldName) : <h2>{name}</h2>}
         <HeartOutlined style={styles.heart} />
       </div>
       <h3>{restaurantName}</h3>
-      {boldDescription !== undefined ? parse(boldDescription) : <p>{description}</p>}
+      {boldDescription ? parse(boldDescription) : <p>{description}</p>}
       <p>
         Expires on
         <strong>{` ${new Date(expirationDate).toDateString()}`}</strong>
