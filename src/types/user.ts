b609477fd2ada89ad1e6034bedfabf4adba98f21
Andrew Zulaybar <andrewzulaybar@gmail.com>
2021-02-21 02:04:54 -0800

    feat(my-promotions): Fetch user's uploaded promotions

---
+++ src/types/user.ts
@@ -0,0 +1,10 @@
+import { Promotion } from './promotion';
+
+export interface User {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  uploadedPromotions: Promotion[];
+  userName: string;
+}
