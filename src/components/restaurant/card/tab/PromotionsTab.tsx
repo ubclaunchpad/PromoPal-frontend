ab25f78e549dabf5fba8acd2a93ea1a223880a0c
Andrew Zulaybar <andrewzulaybar@gmail.com>
2021-03-14 22:43:16 -0700

    refactor(restaurant-card): Resolve type errors

--- src/components/restaurant/card/tab/PromotionsTab.tsx
+++ src/components/restaurant/card/tab/PromotionsTab.tsx
@@ -2,7 +2,7 @@ import './PromotionsTab.css';
 
 import { ClockCircleOutlined } from '@ant-design/icons';
 import { Col, Row, Tabs } from 'antd';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useMemo } from 'react';
 
 import { Promotion } from '../../../../types/promotion';
 import { formatTime } from '../../../../utils/time';
@@ -19,22 +19,11 @@ interface Props {
 
 export default function PromotionsTab(props: Props): ReactElement {
   /**
-   * Orders the (day, promotions) tuples from Sunday to Saturday.
-   */
-  const getOrderedEntries = (): Array<[string, Promotion[]]> => {
-    const order = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-    const pairs = Object.entries(mapPromotionsToDays(props.promotions));
-    return pairs.sort(([a], [b]) => order.indexOf(a) - order.indexOf(b));
-  };
-
-  /**
    * Produces an object that maps days to the promotions that occur on those days.
-   *
-   * @param promotions - The list of promotions to sort
    */
-  const mapPromotionsToDays = (promotions: Promotion[]): DayToPromotions => {
+  const daysToPromotions = useMemo((): DayToPromotions => {
     const daysToPromotions: DayToPromotions = {};
-    promotions.forEach((promotion) => {
+    props.promotions.forEach((promotion) => {
       promotion.schedules.forEach(({ dayOfWeek }) => {
         const abbreviation =
           dayOfWeek.charAt(0).toUpperCase() + dayOfWeek.slice(1, 3).toLowerCase();
@@ -44,13 +33,13 @@ export default function PromotionsTab(props: Props): ReactElement {
       });
     });
     return daysToPromotions;
-  };
+  }, [props.promotions]);
 
   return (
     <Tabs className="tab-promotions" defaultActiveKey="Sun" size="small">
-      {getOrderedEntries().map(([day, promotions]) => (
+      {['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].map((day) => (
         <TabPane tab={day} key={day}>
-          {promotions.map((promotion) => (
+          {daysToPromotions[day].map((promotion) => (
             <Row className="promotion-info">
               <Col span={2}>
                 <ClockCircleOutlined className="clock-icon" />
