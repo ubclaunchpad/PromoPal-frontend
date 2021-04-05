3a9ab6acf71c312d7dab09989f525e02a08b8f63
Andrew Zulaybar <andrewzulaybar@gmail.com>
2021-04-04 18:16:02 -0700

    style: Extract from screens

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,27 +1,16 @@
 import '../index.less';
+import './Login.less';
 
-import React, { CSSProperties, ReactElement, useState } from 'react';
+import React, { ReactElement, useState } from 'react';
 
 import ForgotPasswordCard from '../components/account/ForgotPasswordCard';
 import LoginCard from '../components/account/LoginCard';
 import RegisterCard from '../components/account/RegisterCard';
 
-const styles: { [identifier: string]: CSSProperties } = {
-  container: {
-    backgroundColor: 'white',
-    borderRadius: 20,
-    boxShadow: '0 4px 4px 0 #40333333',
-    color: '#333',
-    margin: 30,
-    overflow: 'auto',
-    padding: 30,
-  },
-};
-
 export default function Login(): ReactElement {
   const [state, setState] = useState<string>('Login');
   return (
-    <div style={styles.container}>
+    <div className="login-container">
       {state === 'Login' && (
         <LoginCard
           onClickForgotPassword={() => setState('Forgot Password')}
--- src/screens/MyAccount.tsx
+++ src/screens/MyAccount.tsx
@@ -1,5 +1,7 @@
+import './MyAccount.less';
+
 import { Col, Row } from 'antd';
-import React, { CSSProperties, ReactElement, useEffect, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 
 import AccountDetails from '../components/account/AccountDetails';
 import AccountPhoto from '../components/account/AccountPhoto';
@@ -16,16 +18,6 @@ const defaultUser: User = {
   username: '',
 };
 
-const styles: { [identifier: string]: CSSProperties } = {
-  body: {
-    backgroundColor: '#FFEDDC',
-    padding: 25,
-    width: '100vw',
-    height: '100vh',
-    display: 'inline-flex',
-  },
-};
-
 export default function MyAccount(): ReactElement {
   const [user, setUser] = useState<User>(defaultUser);
 
@@ -39,7 +31,7 @@ export default function MyAccount(): ReactElement {
   }, []);
 
   return (
-    <Row style={styles.body} justify="space-around">
+    <Row className="account-container" justify="space-around">
       <Col span={4}>
         <AccountPhoto />
       </Col>
